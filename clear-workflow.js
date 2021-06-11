cf8af1d0482abb53810a10ed77f6e53b69c986a0
gamekingv <793470218@qq.com>
2021-06-11 16:37:52 +0800

    Initialize

--- clear-workflow.js
+++ clear-workflow.js
@@ -2,7 +2,8 @@ const got = require('got');
 
 const {
   GITHUB_REPOSITORY: repository,
-  CLEAR_TOKEN: token
+  CLEAR_TOKEN: token,
+  RETAIN_DAY: retain_day
 } = process.env;
 
 const client = got.extend({
@@ -26,40 +27,50 @@ const client = got.extend({
   }
 });
 
+async function deleteWorkflows(page, remain = 0) {
+  const now = Date.now();
+  const { body } = await client.get(`https://api.github.com/repos/${repository}/actions/runs`, {
+    searchParams: {
+      per_page: 100,
+      page
+    }
+  });
+  let workflow_count = body.total_count;
+  let deleted = 0, reach_newest = false;
+  const completed_workflows = body.workflow_runs.filter(workflow => workflow.status === 'completed').reverse();
+  remain += 100 - completed_workflows.length;
+  for (const workflow of completed_workflows) {
+    if (now - new Date(workflow.created_at).getTime() <= retain_day * 24 * 60 * 60 * 1000) {
+      reach_newest = true;
+      break;
+    }
+    try {
+      await client.delete(`https://api.github.com/repos/${repository}/actions/runs/${workflow.id}`);
+      console.log(`删除 ${workflow.id}`);
+      deleted++;
+    }
+    catch (error) {
+      console.log(`删除 ${workflow.id} 失败`);
+      remain++;
+    }
+    await new Promise(res => setTimeout(() => res(), 500));
+  }
+  workflow_count -= deleted;
+  return { workflow_count, remain, reach_newest };
+}
+
 (async () => {
   try {
     const { body } = await client.get(`https://api.github.com/repos/${repository}/actions/runs`);
-    const now = Date.now(), remain_day = 7;
     let workflow_count = body.total_count;
-    let remain = 0, reach_newest = false;
+    let remain = 0, reach_newest = false, page;
     while (workflow_count > 1 && !reach_newest) {
-      let deleted = 0;
-      const { body } = await client.get(`https://api.github.com/repos/${repository}/actions/runs`, {
-        searchParams: {
-          per_page: 100,
-          page: Math.ceil(workflow_count / 100) - Math.ceil(remain / 100)
-        }
-      });
-      workflow_count = body.total_count;
-      const completed_workflows = body.workflow_runs.filter(workflow => workflow.status === 'completed');
-      remain += 100 - completed_workflows.length;
-      for (const workflow of completed_workflows) {
-        if (now - new Date(workflow.created_at).getTime() <= remain_day * 24 * 60 * 60 * 1000) {
-          reach_newest = true;
-          break;
-        }
-        try {
-          await client.delete(`https://api.github.com/repos/${repository}/actions/runs/${workflow.id}`);
-          console.log(`删除 ${workflow.id}`);
-          deleted++;
-        }
-        catch (error) {
-          console.log(`删除 ${workflow.id} 失败`);
-          remain++;
-        }
-        await new Promise(res => setTimeout(() => res(), 500));
-      }
-      workflow_count -= deleted;
+      page = Math.ceil(workflow_count / 100) - Math.ceil(remain / 100);
+      ({ workflow_count, remain, reach_newest } = await deleteWorkflows(page, remain));
+    }
+    if (remain > 0) {
+      await deleteWorkflows(page + 1);
+      await deleteWorkflows(page);
     }
   }
   catch (error) {
